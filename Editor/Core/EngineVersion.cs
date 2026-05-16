accf3230af3e1f125bad5b3a7c4f3f5d6fff0363
LinhengXilan <2642372751@qq.com>
2026-05-16 19:35:37 +0800

    建立项目浏览器窗口并实现窗口调整

---
+++ Editor/Core/EngineVersion.cs
@@ -0,0 +1,13 @@
+﻿/// @file Core/EngineVersion.cs
+/// author LinhengXilan
+/// @version 0.0.0.2
+/// @date 2025-5-16
+
+namespace Editor.Core {
+	public class EngineVersion {
+		public static readonly int MajorVersion = 0;
+		public static readonly int MinorVersion = 0;
+		public static readonly int PatchVersion = 0;
+		public static readonly int BuildVersion = 2;
+	}
+}
---
+++ Editor/ProjectBrowser/ProjectBrowser.xaml.cs
@@ -0,0 +1,30 @@
+﻿/// @file ProjectBrowser/ProjectBrowser.xaml.cs
+/// author LinhengXilan
+/// @version 0.0.0.2
+/// @date 2025-5-16
+
+using Editor.Core;
+using System.Windows;
+
+namespace Editor.ProjectBrowser
+{
+	public partial class ProjectBrowser : Window {
+		public WindowOperator WindowOperator = new();
+
+		public ProjectBrowser() {
+			InitializeComponent();
+		}
+		
+		public void MinimizationButtonClicked(object sender, RoutedEventArgs args) {
+			WindowOperator.Minimize(this);
+		}
+		
+		public void MaximizationButtonClicked(object sender, RoutedEventArgs args) {
+			WindowOperator.Maximize(this);
+		}
+		
+		public void CloseButtonClicked(object sender, RoutedEventArgs args) {
+			Close();
+		}
+	}
+}
