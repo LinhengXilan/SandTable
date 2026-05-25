4ace966033cf57d3a663af2bff0f2e729a01512f
LinhengXilan <2642372751@qq.com>
2026-05-25 14:45:44 +0800

    设计编辑器界面（类似虚幻）

--- Editor/Core/EngineVersion.cs
+++ Editor/Core/EngineVersion.cs
@@ -1,6 +1,6 @@
 ﻿/// @file Core/EngineVersion.cs
 /// author LinhengXilan
-/// @version 0.0.0.15
+/// @version 0.0.0.16
 /// @date 2025-5-25
 
 namespace Editor.Core {
@@ -8,6 +8,6 @@ namespace Editor.Core {
 		public static readonly uint MajorVersion = 0;
 		public static readonly uint MinorVersion = 0;
 		public static readonly uint PatchVersion = 0;
-		public static readonly uint BuildVersion = 15;
+		public static readonly uint BuildVersion = 16;
 	}
 }
