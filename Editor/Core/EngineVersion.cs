2247762a6deceee208974a60d433f05805fabbd7
LinhengXilan <2642372751@qq.com>
2026-06-10 23:45:57 +0800

    为项目浏览器中的子视图添加设计时DataContext

--- Editor/Core/EngineVersion.cs
+++ Editor/Core/EngineVersion.cs
@@ -1,13 +1,13 @@
 ﻿/// @file Core/EngineVersion.cs
 /// @author LinhengXilan
-/// @version 0.0.0.24
-/// @date 2025-6-9
+/// @version 0.0.0.25
+/// @date 2025-6-10
 
 namespace Editor.Core {
 	public class EngineVersion {
 		public static readonly uint MajorVersion = 0;
 		public static readonly uint MinorVersion = 0;
 		public static readonly uint PatchVersion = 0;
-		public static readonly uint BuildVersion = 24;
+		public static readonly uint BuildVersion = 25;
 	}
 }
